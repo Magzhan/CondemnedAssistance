56687edb5ec07969f334d2185f58054b49e10971
kazprogrammist <magzhan93@gmail.com>
2017-04-25 23:14:46 +0600

    Account Controller and its elements have been added

---
+++ CondemnedAssistance/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CondemnedAssistance.Controllers {
+    public class AccountController : Controller {
+        public IActionResult Login() {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register() {
+            return View();
+        }
+    }
+}
