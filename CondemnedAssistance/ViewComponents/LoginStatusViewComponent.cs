65372946516dca43df12a8e32d994903a9043193
kazprogrammist <magzhan93@gmail.com>
2017-05-09 18:35:19 +0600

    Primitive user panel has been added.

---
+++ CondemnedAssistance/ViewComponents/LoginStatusViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using CondemnedAssistance.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CondemnedAssistance.ViewComponents {
+    public class LoginStatusViewComponent : ViewComponent {
+
+        private readonly UserContext _db;
+
+        public LoginStatusViewComponent(UserContext context) {
+            this._db = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() {
+            if (User.Identity.IsAuthenticated) {
+                int currentUser = Convert.ToInt32(HttpContext.User.Identity.Name);
+                User user = await _db.Users.FirstOrDefaultAsync(u => u.Id == currentUser);
+                return View("LoggedIn", user);
+            } else {
+                return View();
+            }
+        }
+    }
+}
