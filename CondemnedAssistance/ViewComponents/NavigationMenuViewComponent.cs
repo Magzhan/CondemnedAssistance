152d0867068eff5ac674f97575a92995685fe825
kazprogrammist <magzhan93@gmail.com>
2017-07-20 04:07:16 +0600

    Unneeded component has been removed. Component for dynamic navigation bar has been created.

---
+++ CondemnedAssistance/ViewComponents/NavigationMenuViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CondemnedAssistance.ViewComponents {
+    public class NavigationMenuViewComponent : ViewComponent {
+
+        public async Task<IViewComponentResult> InvokeAsync() {
+            if (User.Identity.IsAuthenticated) {
+                string role = HttpContext.User.FindFirst(c => c.Type == ClaimsIdentity.DefaultRoleClaimType).Value;
+                string view = "UserView";
+                switch (role) {
+                    case "3":
+                        view = "FullView";
+                        break;
+                    case "1":
+                        view = "UserView";
+                        break;
+                    case "2":
+                        view = "AdminView";
+                        break;
+                    case "4":
+                        view = "CounselorView";
+                        break;
+                    default:
+                        view = "UserView";
+                        break;
+                }
+                return View(view);
+            }
+            else {
+                return View();
+            }
+        }
+    }
+}
